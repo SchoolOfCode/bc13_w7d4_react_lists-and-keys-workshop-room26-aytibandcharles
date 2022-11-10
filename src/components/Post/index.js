ddb118b04066509988797cca5664a525e55dcb2b
Muhammed Atyib Ali <aatyib@gmail.com>
2022-11-10 16:43:48 +0000

    tasks completed

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
 import './index.css';
-
-function Post({title}) {
-  return <article>{title}</article>;
+function Post( props) {
+  return <article>
+    <h1>{props.title}</h1>
+    <date>{props.date}</date>
+    <h2>{props.author}</h2>
+    <p>{props.text}</p>
+    {props.highlights.map((highlight) => <li key={highlight.indexOf()}>highlight</li>)}
+    <img src = {props.image.link} alt={props.image.alt}/>
+  </article>;
 }
-
 export default Post;
